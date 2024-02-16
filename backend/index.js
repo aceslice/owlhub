4c823dcad51efef62d441c4241ab76d628cf143c
Gideon Appau <gideonad.codez@gmail.com>
2024-02-15 17:42:14 -0800

    Update import statement in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userModel = require("./models/userModel");
-import api from "./api";
+import api from "./api/app";
 require("dotenv").config();
 const app = express();
 app.use(cors());
